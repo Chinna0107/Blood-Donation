fd2b786c1c685cf43fa72e397d5f01bc1fa15d0b
chinna0107 <kancharlahemanth89@gmail.com>
2026-02-05 22:00:31 +0530

    Production 1.3

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const isProduction = window.location.hostname !== 'localhost';
 
 const config = {
   API_BASE_URL: isProduction 
-    ? 'https://blood-donation-backend-2kmfdon96-chinna0107s-projects.vercel.app/api'
+    ? 'https://blood-donation-backend-eight.vercel.app/'
     : 'http://localhost:5000/api'
 };
 
